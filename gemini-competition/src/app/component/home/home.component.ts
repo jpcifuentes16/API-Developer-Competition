20b593d25724d4a4ee86fdbaa1dae339eb1cbd0e
josepablito16 <josepablocif16@gmail.com>
2024-06-26 21:02:32 -0600

    Simple version of home screen implemented

--- gemini-competition/src/app/component/home/home.component.ts
+++ gemini-competition/src/app/component/home/home.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  interviewCode: string = '';
+  isButtonEnabled: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onInputChange() {
+    this.isButtonEnabled = this.interviewCode.trim().length > 0;
   }
 
+  ingresar() {
+    // Lógica para ingresar con el código de entrevista
+    console.log('Ingresar con el código:', this.interviewCode);
+  }
+
+  nuevaEntrevista() {
+    // Lógica para nueva entrevista
+    console.log('Nueva entrevista');
+  }
+
+  verResultados() {
+    // Lógica para ver resultados
+    console.log('Ver resultados');
+  }
 }
