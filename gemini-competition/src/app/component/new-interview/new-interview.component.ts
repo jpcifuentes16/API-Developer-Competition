a8caa0794d1664e605429f81a53a2097c4d61e16
josepablito16 <josepablocif16@gmail.com>
2024-08-01 21:51:33 -0600

    visual enhancements and elimination of burned-in data in the creation of an interview are implemented

--- gemini-competition/src/app/component/new-interview/new-interview.component.ts
+++ gemini-competition/src/app/component/new-interview/new-interview.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from 'src/app/shared/data.service';
 
-
-
 interface IInterviewTemplate {
   configuration: string;
   id: string;
@@ -17,45 +15,48 @@ interface IInterviewTemplate {
 })
 export class NewInterviewComponent {
   templates: IInterviewTemplate[] = [];
+  isModalOpen = false;
+  inputName = '';
+  currentTemplateIndex: number = 0;
 
+  constructor(private data: DataService, private router: Router) {}
 
-
-
-  constructor(private data: DataService,  private router : Router){}
-
-
-  ngOnInit(): void
-  {
-    this.getAllStudents();
+  ngOnInit(): void {
+    this.getAllTemplates();
   }
 
-
-  getAllStudents() {
-
+  getAllTemplates() {
     this.data.getTemplates().subscribe(res => {
-
-      this.templates = res;      
-
+      this.templates = res;
     }, err => {
-      alert('Error while fetching student data');
-    })
-
+      alert('Error al obtener los datos de las plantillas');
+    });
   }
-
-
 
   addTemplate() {
     this.router.navigate(['/new-template']);
   }
 
-  generateLink(index: number) {
-    this.data.createInterview(this.templates[index].id, "Juanito Perez").subscribe( () => {
-      alert(`Generating link for ${this.templates[index].id}`);
-    }, err => {
-      alert(`Error generating link: ${err}`);
-    });
+  openModal(index: number) {
+    this.currentTemplateIndex = index;
+    this.isModalOpen = true;
+  }
 
+  closeModal() {
+    this.isModalOpen = false;
+    this.inputName = '';
+    this.currentTemplateIndex = 0;
+  }
 
-    // Lógica para generar el enlace aquí
+  submitName() {
+    if (this.inputName.trim()) {
+      this.data.createInterview(this.templates[this.currentTemplateIndex].id, this.inputName).subscribe(() => {
+        this.closeModal();
+      }, err => {
+        alert(`Error al generar el enlace: ${err}`);
+      });
+    } else {
+      alert('Por favor, ingrese un nombre válido.');
+    }
   }
 }
