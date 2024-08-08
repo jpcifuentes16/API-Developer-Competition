1c8a8fda682ec1193452d72f501344820b185964
josepablito16 <josepablocif16@gmail.com>
2024-08-07 23:30:12 -0600

    tempui screen logic is implemented, which is the ui of the interview.

--- gemini-competition/src/app/component/temp-ui/temp-ui.component.ts
+++ gemini-competition/src/app/component/temp-ui/temp-ui.component.ts
@@ -7,14 +7,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TempUIComponent implements OnInit {
 
-  participants = [
-    { initial: 'N', name: 'Nate' },
-    { initial: 'J', name: 'Jose Sanchez' }
-  ];
+  nameCandidate: string = "José Cifuentes";
+  timer: number = 0;
+  interval: any;
+  id: string = "E0wSe7uENdvzX9P5uHrr";
+  formattedId: string = "";
+  isUserTurn = false;
 
-  constructor() { }
+  constructor() {
+    this.formattedId = this.formatId(this.id);
+  }
 
   ngOnInit(): void {
+    this.startTimer();
+  }
+
+  ngOnDestroy(): void {
+    this.clearTimer();
+  }
+
+  formatId(id: string): string {
+    if (id.length >= 13) {
+      return `${id.substring(0, 3)}-${id.substring(3, 7)}-${id.substring(7, 10)}`;
+    }
+    return id;
+  }
+
+  startTimer(): void {
+    this.interval = setInterval(() => {
+      this.timer++;
+    }, 1000); // Incrementa el cronómetro cada segundo
+  }
+
+  clearTimer(): void {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+
+  getFirstLetterUpperCase(): string {
+    if (this.nameCandidate && this.nameCandidate.length > 0) {
+      return this.nameCandidate.charAt(0).toUpperCase();
+    }
+    return '';
+  }
+
+
+  formatTimer(): string {
+    const hours = Math.floor(this.timer / 3600);
+    const minutes = Math.floor((this.timer % 3600) / 60);
+    const seconds = this.timer % 60;
+
+    if (hours > 0) {
+      return `${this.pad(hours)}:${this.pad(minutes)}:${this.pad(seconds)}`;
+    } else {
+      return `${this.pad(minutes)}:${this.pad(seconds)}`;
+    }
+  }
+
+  pad(value: number): string {
+    return value.toString().padStart(2, '0');
   }
 
 }
