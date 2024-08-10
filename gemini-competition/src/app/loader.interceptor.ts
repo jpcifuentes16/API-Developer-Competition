7392b95de6dfa898ba2042c1b92637e8b974d153
josepablito16 <josepablocif16@gmail.com>
2024-08-09 22:36:39 -0600

    logic is implemented so that the loader is not shown in the interview

--- gemini-competition/src/app/loader.interceptor.ts
+++ gemini-competition/src/app/loader.interceptor.ts
@@ -3,12 +3,10 @@ import {
   HttpEvent,
   HttpInterceptor,
   HttpHandler,
-  HttpRequest,
-  HttpResponse,
-  HttpErrorResponse
+  HttpRequest
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { tap, finalize } from 'rxjs/operators';
+import { finalize } from 'rxjs/operators';
 import { LoaderService } from './loader.service';
 
 @Injectable()
@@ -16,7 +14,11 @@ export class LoaderInterceptor implements HttpInterceptor {
   constructor(private loaderService: LoaderService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    this.loaderService.show();
+    // No mostrar el loader si la URL contiene 'interview'
+    if (!req.url.includes('/interview/')) {
+      this.loaderService.show();
+    }
+
     return next.handle(req).pipe(
       finalize(() => this.loaderService.hide())
     );
